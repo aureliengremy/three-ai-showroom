4d67266b7cdc06941c3eade00f070631f70a2c17
Aurelien Gremy <gremy.aurelien@gmail.com>
2023-04-14 13:30:15 -0400

    feat(BackDrop,CameraRig,ElementThreeDim) create some element to use on ThreeJS environnement > Canvas

--- src/canvas/index.jsx
+++ src/canvas/index.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { Canvas } from "@react-three/fiber";
+import { Environment, Center } from "@react-three/drei";
+import ElementThreeDim from "./ElementThreeDim";
+// import { BackDrop } from "./BackDrop";
+// import { CameraRig } from "./CameraRig";
+import { lazy, Suspense } from 'react'
 
-const Canvas = () => {
+const CanvasModel = () => {
+
+  // const ModelComponent = lazy(() => import('./ElementThreeDim'))
+  
   return (
-    <div>Canvas</div>
+    // <Suspense fallback={"loading"}>
+      <Canvas>
+        {/* <AmbiantLight intensity={0.5} /> */}
+        <Environment preset="city" />
+        {/* <CameraRig>
+          <BackDrop /> */}
+          <Center>
+            {/* <ModelComponent /> */}
+            <ElementThreeDim />
+          </Center>
+        {/* </CameraRig> */}
+      </Canvas>
+    // </Suspense>
   )
 }
 
-export default Canvas
+export default CanvasModel
