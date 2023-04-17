75b88f3605a46013b6db9d5ebad9e6f5868a43e8
Aurelien Gremy <gremy.aurelien@gmail.com>
2023-04-17 17:51:48 -0400

    add the /client folder as it was before the conflit

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import Canvas from './canvas'
+import Customizer from './pages/Customizer'
+import Home from './pages/Home'
+
+function App() {
+
+  return (
+    <main className="app transition-all ease-in">
+
+      <Home />
+      <Customizer />
+      <Canvas />
+    </main>
+  )
+}
+
+export default App
