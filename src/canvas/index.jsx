e9ef41c496b92252e57c464bc1631307bf7e938f
Aurelien Gremy <gremy.aurelien@gmail.com>
2023-04-17 11:26:02 -0400

    feat(/canva -> index) add the backDrop light and shadowss

--- src/canvas/index.jsx
+++ src/canvas/index.jsx
@@ -1,7 +1,8 @@
 import { Canvas } from "@react-three/fiber";
 import { Environment, Center } from "@react-three/drei";
 import ElementTShirt from "./ElementTShirt";
-// import BackDrop from "./BackDrop";
+import ElementGuitarPick from './ElementGuitarPick'
+import BackDrop from "./BackDrop";
 import CameraRig from "./CameraRig";
 import { lazy, Suspense } from 'react'
 
@@ -11,11 +12,16 @@ const CanvasModel = () => {
   
   return (
     // <Suspense fallback={"loading"}>
-      <Canvas>
-        {/* <ambiantLight intensity={0.5} /> */}
+      <Canvas
+        shadows
+        camera={{ position: [0, 0, 0], fov:25  }}
+        gl={{ preserveDrawingBuffer: true }}
+        className="w-full max-w-full h-full transition-all ease-in"
+      >
+        <ambientLight intensity={0.5} />
         <Environment preset="city" />
           <CameraRig>
-          {/* <BackDrop /> */}
+          <BackDrop />
             <Center>
               {/* <ModelComponent /> */}
               <ElementTShirt />
