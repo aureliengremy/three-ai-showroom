4be3aab063511a036476bddbcdf58dc0756c3e2c
Aurelien Gremy <gremy.aurelien@gmail.com>
2023-04-18 15:18:06 -0400

    files(/assets) clear and remove icone and image used

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -3,7 +3,8 @@ import fileIcon from "./file.png";
 import swatch from "./swatch.png";
 import download from "./download.png";
 
-import logoShirt from "./logo-tshirt.png";
-import stylishShirt from "./stylish-tshirt.png";
+import logoPick from "./pick-logo-64.png";
+import stylishPick from "./pick-full-96.png";
+import closeIcone from './close-64.png'
 
-export { ai, fileIcon, swatch, download, logoShirt, stylishShirt };
+export { ai, fileIcon, swatch, download, logoPick, stylishPick, closeIcone };
